834c2f5ec444fda392a1a85a2855ac27315dfd56
Saif <saifmohd003@gmail.com>
2026-03-14 01:04:45 +0530

    Initial Commit

---
+++ Snabbit Assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snabbit Assesment
+//
+//  Created by Muhammad Saif on 14/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
